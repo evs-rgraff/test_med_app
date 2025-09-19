fa985bc2eb9988ed8dedb5e7c67e29e04a246abf
evs-rgraff <rgraff@everspringpartners.com>
2025-09-19 11:24:08 -0400

    mongo setup

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:OisAUaBHRpqO4jPA95nrD5Ui@172.21.53.6:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
