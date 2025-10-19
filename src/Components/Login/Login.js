795c643a678457984f9b712b4232f4f64799868b
evs-rgraff <rgraff@everspringpartners.com>
2025-10-19 10:22:14 -0400

    add css to Login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import './Login.css';
 import { API_URL } from '../../config';
 
 const Login = ({ onLogin }) => {
