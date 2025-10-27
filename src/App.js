8f2dcf0fef466a1e5aff878b6e5e537c8915d3de
evs-rgraff <rgraff@everspringpartners.com>
2025-10-27 15:30:54 -0400

    convert landing page to react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
+import Landing_page from './Components/Landing_Page/Landing_page';
 
 // Temporary placeholder components (so your app compiles)
-function Home() {
-  return <div style={{ padding: 20 }}>Home Page</div>;
-}
-
 function Appointments() {
   return <div style={{ padding: 20 }}>Appointments Page</div>;
 }
@@ -25,7 +22,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Landing_page />} />
           <Route path="/appointments" element={<Appointments />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
